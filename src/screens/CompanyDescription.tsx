6585c1944969262945168320ba95fb7545dc41fa
Jesus Muller <jhzapata.dev@gmail.com>
2024-01-26 15:55:18 -0500

    header fixed and company logos

--- src/screens/CompanyDescription.tsx
+++ src/screens/CompanyDescription.tsx
@@ -1,5 +1,124 @@
+import "./company-description.css";
 const CompanyDescription = () => {
-    return <div>hola soy la descripcion de la empresa</div>
-}
+  return (
+    <div className="company-description">
+      hola soy la descripcion de la empresa Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Veniam quas mollitia doloribus eos
+      voluptatem nihil assumenda dolorum consequatur in itaque, maxime alias
+      autem veritatis architecto, ut quo numquam ullam sapiente.
+       Lorem ipsum
+      dolor sit amet consectetur, adipisicing elit. Voluptates non ratione autem
+      deleniti adipisci tempora et, error reiciendis necessitatibus commodi
+      suscipit nobis consequuntur in nihil illum consectetur rerum omnis
+      voluptatem? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+
+      Numquam possimus, eaque obcaecati blanditiis dolores exercitationem
+      aliquam earum error laborum voluptatum perspiciatis, placeat tenetur in
+      laudantium dolor enim? Repellat, possimus similique. Lorem ipsum dolor sit
+      amet consectetur adipisicing elit. Quis, aspernatur repellendus illum
+      iste, expedita cumque, a minus ad quidem vero laudantium quam porro
+      itaque. Neque aspernatur culpa modi eum atque! Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Accusantium ipsum itaque est sunt nesciunt
+      molestias quos officia eos id hic iure a temporibus laudantium maiores cum
+      provident adipisci, sequi eius.
+      hola soy la descripcion de la empresa Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Veniam quas mollitia doloribus eos
+      voluptatem nihil assumenda dolorum consequatur in itaque, maxime alias
+      autem veritatis architecto, ut quo numquam ullam sapiente.
+       Lorem ipsum
+      dolor sit amet consectetur, adipisicing elit. Voluptates non ratione autem
+      deleniti adipisci tempora et, error reiciendis necessitatibus commodi
+      suscipit nobis consequuntur in nihil illum consectetur rerum omnis
+      voluptatem? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+
+      Numquam possimus, eaque obcaecati blanditiis dolores exercitationem
+      aliquam earum error laborum voluptatum perspiciatis, placeat tenetur in
+      laudantium dolor enim? Repellat, possimus similique. Lorem ipsum dolor sit
+      amet consectetur adipisicing elit. Quis, aspernatur repellendus illum
+      iste, expedita cumque, a minus ad quidem vero laudantium quam porro
+      itaque. Neque aspernatur culpa modi eum atque! Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Accusantium ipsum itaque est sunt nesciunt
+      molestias quos officia eos id hic iure a temporibus laudantium maiores cum
+      provident adipisci, sequi eius.
+      hola soy la descripcion de la empresa Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Veniam quas mollitia doloribus eos
+      voluptatem nihil assumenda dolorum consequatur in itaque, maxime alias
+      autem veritatis architecto, ut quo numquam ullam sapiente.
+       Lorem ipsum
+      dolor sit amet consectetur, adipisicing elit. Voluptates non ratione autem
+      deleniti adipisci tempora et, error reiciendis necessitatibus commodi
+      suscipit nobis consequuntur in nihil illum consectetur rerum omnis
+      voluptatem? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+
+      Numquam possimus, eaque obcaecati blanditiis dolores exercitationem
+      aliquam earum error laborum voluptatum perspiciatis, placeat tenetur in
+      laudantium dolor enim? Repellat, possimus similique. Lorem ipsum dolor sit
+      amet consectetur adipisicing elit. Quis, aspernatur repellendus illum
+      iste, expedita cumque, a minus ad quidem vero laudantium quam porro
+      itaque. Neque aspernatur culpa modi eum atque! Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Accusantium ipsum itaque est sunt nesciunt
+      molestias quos officia eos id hic iure a temporibus laudantium maiores cum
+      provident adipisci, sequi eius.
+      hola soy la descripcion de la empresa Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Veniam quas mollitia doloribus eos
+      voluptatem nihil assumenda dolorum consequatur in itaque, maxime alias
+      autem veritatis architecto, ut quo numquam ullam sapiente.
+       Lorem ipsum
+      dolor sit amet consectetur, adipisicing elit. Voluptates non ratione autem
+      deleniti adipisci tempora et, error reiciendis necessitatibus commodi
+      suscipit nobis consequuntur in nihil illum consectetur rerum omnis
+      voluptatem? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+
+      Numquam possimus, eaque obcaecati blanditiis dolores exercitationem
+      aliquam earum error laborum voluptatum perspiciatis, placeat tenetur in
+      laudantium dolor enim? Repellat, possimus similique. Lorem ipsum dolor sit
+      amet consectetur adipisicing elit. Quis, aspernatur repellendus illum
+      iste, expedita cumque, a minus ad quidem vero laudantium quam porro
+      itaque. Neque aspernatur culpa modi eum atque! Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Accusantium ipsum itaque est sunt nesciunt
+      molestias quos officia eos id hic iure a temporibus laudantium maiores cum
+      provident adipisci, sequi eius.
+
+
+      hola soy la descripcion de la empresa Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Veniam quas mollitia doloribus eos
+      voluptatem nihil assumenda dolorum consequatur in itaque, maxime alias
+      autem veritatis architecto, ut quo numquam ullam sapiente.
+       Lorem ipsum
+      dolor sit amet consectetur, adipisicing elit. Voluptates non ratione autem
+      deleniti adipisci tempora et, error reiciendis necessitatibus commodi
+      suscipit nobis consequuntur in nihil illum consectetur rerum omnis
+      voluptatem? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+
+      Numquam possimus, eaque obcaecati blanditiis dolores exercitationem
+      aliquam earum error laborum voluptatum perspiciatis, placeat tenetur in
+      laudantium dolor enim? Repellat, possimus similique. Lorem ipsum dolor sit
+      amet consectetur adipisicing elit. Quis, aspernatur repellendus illum
+      iste, expedita cumque, a minus ad quidem vero laudantium quam porro
+      itaque. Neque aspernatur culpa modi eum atque! Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Accusantium ipsum itaque est sunt nesciunt
+      molestias quos officia eos id hic iure a temporibus laudantium maiores cum
+      provident adipisci, sequi eius.
+      hola soy la descripcion de la empresa Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Veniam quas mollitia doloribus eos
+      voluptatem nihil assumenda dolorum consequatur in itaque, maxime alias
+      autem veritatis architecto, ut quo numquam ullam sapiente.
+       Lorem ipsum
+      dolor sit amet consectetur, adipisicing elit. Voluptates non ratione autem
+      deleniti adipisci tempora et, error reiciendis necessitatibus commodi
+      suscipit nobis consequuntur in nihil illum consectetur rerum omnis
+      voluptatem? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+
+      Numquam possimus, eaque obcaecati blanditiis dolores exercitationem
+      aliquam earum error laborum voluptatum perspiciatis, placeat tenetur in
+      laudantium dolor enim? Repellat, possimus similique. Lorem ipsum dolor sit
+      amet consectetur adipisicing elit. Quis, aspernatur repellendus illum
+      iste, expedita cumque, a minus ad quidem vero laudantium quam porro
+      
+      molestias quos officia eos id hic iure a temporibus laudantium maiores cum
+      provident adipisci, sequi eius.
+    </div>
+  );
+};
 
 export default CompanyDescription;
