d1e95bba037c0adbcf50d2a61b37644d2756b8fb
Jesus Muller <jhzapata.dev@gmail.com>
2024-02-02 22:41:47 -0300

    thomson reuters content

---
+++ src/constants/details.js
@@ -0,0 +1,32 @@
+const details = [
+  {
+    id: 2013,
+    name: "Thomson Reuters",
+    description:
+      "Thomson Reuters / La Ley Argentina es una empresa dedicada principalmente a la venta de libros para abogados y productos digitales que facilitan la búsqueda dentro de una extensa base de datos de jurisprudencia, así como el enriquecimiento de los textos encontrados.",
+    myExperience: [
+      "Thomson fue la primera empresa que me brindó la oportunidad de adentrarme en el mundo del desarrollo y donde tuve el placer de conocer a grandes personas. Aquí, aprendí rápidamente la importancia del trabajo en equipo y cómo adaptarse a las presiones diarias de la industria del desarrollo.",
+      "En Thomson, gracias a mi destacado desempeño, fui ascendido a desarrollador SSR en mis primeros 6 meses de trabajo. Siempre me consideraron para enfrentar los mayores desafíos dentro del proyecto. Además, tuve mi primera experiencia liderando un proyecto pequeño, Westlaw India, donde adquirí una valiosa experiencia al interactuar directamente con el cliente y al contribuir en la elaboración y refinamiento de los requisitos.",
+      "Aprendí mucho sobre habilidades blandas, metodologías ágiles de trabajo como Scrum o Kanban, el manejo de Jira y la estimación en story points, entre otros aspectos."
+    ],
+    technologies: [
+      "HTML4",
+      "XHTML",
+      "CSS",
+      "Javascript",
+      "Jquery",
+      "Apache velocicty",
+      "XSLT",
+      "Ajax",
+      "TFS"
+    ],
+    projects: [
+      "La Ley (Argentina, Brasil, Uruguay, Chile, Perú)",
+      "Westlaw UK",
+      "Westlaw India",
+    ],
+    workedTime: { from: "2013", to: "2016" },
+  },
+];
+
+export default details;
--- src/screens/CompanyDescription.tsx
+++ src/screens/CompanyDescription.tsx
@@ -1,122 +1,40 @@
 import "./company-description.css";
+import details from "../constants/details";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+
 const CompanyDescription = () => {
+  const { id } = useParams();
+
+  const [company, setCompany] = useState<{
+    id: number;
+    name: string;
+    description: string;
+    myExperience: string[];
+    technologies: string[];
+    projects: string[];
+    workedTime: { from: string, to: string};
+  }>();
+
+  useEffect(() => {
+    const comp = details.find((detail) => detail.id === Number(id));
+    setCompany(comp);
+  }, [id]);
+
   return (
     <div className="company-description">
-      hola soy la descripcion de la empresa Lorem ipsum dolor sit amet
-      consectetur adipisicing elit. Veniam quas mollitia doloribus eos
-      voluptatem nihil assumenda dolorum consequatur in itaque, maxime alias
-      autem veritatis architecto, ut quo numquam ullam sapiente.
-       Lorem ipsum
-      dolor sit amet consectetur, adipisicing elit. Voluptates non ratione autem
-      deleniti adipisci tempora et, error reiciendis necessitatibus commodi
-      suscipit nobis consequuntur in nihil illum consectetur rerum omnis
-      voluptatem? Lorem ipsum dolor sit amet consectetur adipisicing elit.
-
-      Numquam possimus, eaque obcaecati blanditiis dolores exercitationem
-      aliquam earum error laborum voluptatum perspiciatis, placeat tenetur in
-      laudantium dolor enim? Repellat, possimus similique. Lorem ipsum dolor sit
-      amet consectetur adipisicing elit. Quis, aspernatur repellendus illum
-      iste, expedita cumque, a minus ad quidem vero laudantium quam porro
-      itaque. Neque aspernatur culpa modi eum atque! Lorem ipsum dolor sit amet
-      consectetur adipisicing elit. Accusantium ipsum itaque est sunt nesciunt
-      molestias quos officia eos id hic iure a temporibus laudantium maiores cum
-      provident adipisci, sequi eius.
-      hola soy la descripcion de la empresa Lorem ipsum dolor sit amet
-      consectetur adipisicing elit. Veniam quas mollitia doloribus eos
-      voluptatem nihil assumenda dolorum consequatur in itaque, maxime alias
-      autem veritatis architecto, ut quo numquam ullam sapiente.
-       Lorem ipsum
-      dolor sit amet consectetur, adipisicing elit. Voluptates non ratione autem
-      deleniti adipisci tempora et, error reiciendis necessitatibus commodi
-      suscipit nobis consequuntur in nihil illum consectetur rerum omnis
-      voluptatem? Lorem ipsum dolor sit amet consectetur adipisicing elit.
-
-      Numquam possimus, eaque obcaecati blanditiis dolores exercitationem
-      aliquam earum error laborum voluptatum perspiciatis, placeat tenetur in
-      laudantium dolor enim? Repellat, possimus similique. Lorem ipsum dolor sit
-      amet consectetur adipisicing elit. Quis, aspernatur repellendus illum
-      iste, expedita cumque, a minus ad quidem vero laudantium quam porro
-      itaque. Neque aspernatur culpa modi eum atque! Lorem ipsum dolor sit amet
-      consectetur adipisicing elit. Accusantium ipsum itaque est sunt nesciunt
-      molestias quos officia eos id hic iure a temporibus laudantium maiores cum
-      provident adipisci, sequi eius.
-      hola soy la descripcion de la empresa Lorem ipsum dolor sit amet
-      consectetur adipisicing elit. Veniam quas mollitia doloribus eos
-      voluptatem nihil assumenda dolorum consequatur in itaque, maxime alias
-      autem veritatis architecto, ut quo numquam ullam sapiente.
-       Lorem ipsum
-      dolor sit amet consectetur, adipisicing elit. Voluptates non ratione autem
-      deleniti adipisci tempora et, error reiciendis necessitatibus commodi
-      suscipit nobis consequuntur in nihil illum consectetur rerum omnis
-      voluptatem? Lorem ipsum dolor sit amet consectetur adipisicing elit.
-
-      Numquam possimus, eaque obcaecati blanditiis dolores exercitationem
-      aliquam earum error laborum voluptatum perspiciatis, placeat tenetur in
-      laudantium dolor enim? Repellat, possimus similique. Lorem ipsum dolor sit
-      amet consectetur adipisicing elit. Quis, aspernatur repellendus illum
-      iste, expedita cumque, a minus ad quidem vero laudantium quam porro
-      itaque. Neque aspernatur culpa modi eum atque! Lorem ipsum dolor sit amet
-      consectetur adipisicing elit. Accusantium ipsum itaque est sunt nesciunt
-      molestias quos officia eos id hic iure a temporibus laudantium maiores cum
-      provident adipisci, sequi eius.
-      hola soy la descripcion de la empresa Lorem ipsum dolor sit amet
-      consectetur adipisicing elit. Veniam quas mollitia doloribus eos
-      voluptatem nihil assumenda dolorum consequatur in itaque, maxime alias
-      autem veritatis architecto, ut quo numquam ullam sapiente.
-       Lorem ipsum
-      dolor sit amet consectetur, adipisicing elit. Voluptates non ratione autem
-      deleniti adipisci tempora et, error reiciendis necessitatibus commodi
-      suscipit nobis consequuntur in nihil illum consectetur rerum omnis
-      voluptatem? Lorem ipsum dolor sit amet consectetur adipisicing elit.
-
-      Numquam possimus, eaque obcaecati blanditiis dolores exercitationem
-      aliquam earum error laborum voluptatum perspiciatis, placeat tenetur in
-      laudantium dolor enim? Repellat, possimus similique. Lorem ipsum dolor sit
-      amet consectetur adipisicing elit. Quis, aspernatur repellendus illum
-      iste, expedita cumque, a minus ad quidem vero laudantium quam porro
-      itaque. Neque aspernatur culpa modi eum atque! Lorem ipsum dolor sit amet
-      consectetur adipisicing elit. Accusantium ipsum itaque est sunt nesciunt
-      molestias quos officia eos id hic iure a temporibus laudantium maiores cum
-      provident adipisci, sequi eius.
-
-
-      hola soy la descripcion de la empresa Lorem ipsum dolor sit amet
-      consectetur adipisicing elit. Veniam quas mollitia doloribus eos
-      voluptatem nihil assumenda dolorum consequatur in itaque, maxime alias
-      autem veritatis architecto, ut quo numquam ullam sapiente.
-       Lorem ipsum
-      dolor sit amet consectetur, adipisicing elit. Voluptates non ratione autem
-      deleniti adipisci tempora et, error reiciendis necessitatibus commodi
-      suscipit nobis consequuntur in nihil illum consectetur rerum omnis
-      voluptatem? Lorem ipsum dolor sit amet consectetur adipisicing elit.
-
-      Numquam possimus, eaque obcaecati blanditiis dolores exercitationem
-      aliquam earum error laborum voluptatum perspiciatis, placeat tenetur in
-      laudantium dolor enim? Repellat, possimus similique. Lorem ipsum dolor sit
-      amet consectetur adipisicing elit. Quis, aspernatur repellendus illum
-      iste, expedita cumque, a minus ad quidem vero laudantium quam porro
-      itaque. Neque aspernatur culpa modi eum atque! Lorem ipsum dolor sit amet
-      consectetur adipisicing elit. Accusantium ipsum itaque est sunt nesciunt
-      molestias quos officia eos id hic iure a temporibus laudantium maiores cum
-      provident adipisci, sequi eius.
-      hola soy la descripcion de la empresa Lorem ipsum dolor sit amet
-      consectetur adipisicing elit. Veniam quas mollitia doloribus eos
-      voluptatem nihil assumenda dolorum consequatur in itaque, maxime alias
-      autem veritatis architecto, ut quo numquam ullam sapiente.
-       Lorem ipsum
-      dolor sit amet consectetur, adipisicing elit. Voluptates non ratione autem
-      deleniti adipisci tempora et, error reiciendis necessitatibus commodi
-      suscipit nobis consequuntur in nihil illum consectetur rerum omnis
-      voluptatem? Lorem ipsum dolor sit amet consectetur adipisicing elit.
-
-      Numquam possimus, eaque obcaecati blanditiis dolores exercitationem
-      aliquam earum error laborum voluptatum perspiciatis, placeat tenetur in
-      laudantium dolor enim? Repellat, possimus similique. Lorem ipsum dolor sit
-      amet consectetur adipisicing elit. Quis, aspernatur repellendus illum
-      iste, expedita cumque, a minus ad quidem vero laudantium quam porro
-      
-      molestias quos officia eos id hic iure a temporibus laudantium maiores cum
-      provident adipisci, sequi eius.
+      {company && (
+        <>
+          <h2>{company.name}</h2>
+          <p>{company.description}</p>
+          {company.myExperience.map((paragraph) => (
+            <p>{paragraph}</p>
+          ))}
+          <p>Trabajé con las siguientes tecnologías: {company.technologies.join(", ")}.</p>
+          <p>Proyectos en los que participé: {company.projects.join(", ")}.</p>
+          <p>Tiempo trabajado: {company.workedTime.from} hasta {company.workedTime.to}</p>
+        </>
+      )}
     </div>
   );
 };
